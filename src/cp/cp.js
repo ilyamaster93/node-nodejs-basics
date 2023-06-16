e1c6e56b390e3d5d40c5e7225334e682a9d194ec
Ilya Shchukin <i.shchukin@godeltech.com>
2023-06-17 00:03:07 +0200

    feat: implement solution for cp subtusk

--- src/cp/cp.js
+++ src/cp/cp.js
@@ -1,6 +1,14 @@
+import { fork } from 'child_process';
+
+const PATH = './src/cp/files/script.js';
+
 const spawnChildProcess = async (args) => {
     // Write your code here
+    const childProcess = fork(PATH, args, { silent: true });
+
+    process.stdin.pipe(childProcess.stdin);
+    childProcess.stdout.pipe(process.stdout);
 };
 
 // Put your arguments in function call to test this functionality
-spawnChildProcess( /* [someArgument1, someArgument2, ...] */);
+spawnChildProcess(['someArgument1', 'someArgument2', 'someArgument3']);
