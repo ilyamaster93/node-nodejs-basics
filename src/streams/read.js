db2c99410234bb06a4773e27a09e30f25e8996a0
Ilya Shchukin <i.shchukin@godeltech.com>
2023-06-16 22:15:25 +0200

    feat: implement solution for streams subtusk

--- src/streams/read.js
+++ src/streams/read.js
@@ -1,5 +1,10 @@
+import fs from 'fs';
+
 const read = async () => {
     // Write your code here 
+    let filePath = './src/streams/files/fileToRead.txt'
+    const stream = fs.createReadStream(filePath);
+    stream.pipe(process.stdout);
 };
 
 await read();
